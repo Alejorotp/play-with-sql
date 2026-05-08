821fd7864d77325d5285b4df940a69cb357a64fe
amyepes <amyepes@uninorte.edu.co>
2026-05-07 23:24:30 -0500

    updated the queue to use Bullmq
    There were a few errors in the database part so it wont work with the docker implementation, is fixed now, and the queue module was not using real bullmq, is fixed too.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './infrastructure/database/database.module';
+import { QueueModule } from './infrastructure/queue/queue.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, QueueModule],
   controllers: [AppController],
   providers: [AppService],
 })

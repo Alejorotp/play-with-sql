6e1915f25111b646cb1bb6ba368fcd64608a582e
Alejandro Toro <alejandropinedo@uninorte.edu.co>
2026-05-07 23:40:31 -0500

    Fixed dependencies issues

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -18,10 +18,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: configService.get<string>('DB_SYNCHRONIZE', 'false') === 'true',
         logging: configService.get<string>('DB_LOGGING', 'false') === 'true',
         autoLoadEntities: true,
-        entities: [__dirname + '/../../../**/*.entity{.ts,.js}'],
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       }),
     }),
   ],
   exports: [TypeOrmModule],
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
